34a5134e0cb9fde630d00ee8e94a38551bb2c293
zurum <zurumogbonda@gmail.com>
2022-06-26 19:02:57 +0100

    initial commit

---
+++ Product-service-event-driven/src/main/java/com/zurum/Productserviceeventdriven/ProductServiceEventDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.zurum.Productserviceeventdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceEventDrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceEventDrivenApplication.class, args);
+	}
+
+}
